573302b12fdcaa94564020b2ee069af6097b7c29
Bullets-jyr <jyr@bullets.co.kr>
2023-11-29 11:55:06 +0900

    02. View와 ViewController

--- MyUIKit/ViewController.swift
+++ MyUIKit/ViewController.swift
@@ -9,12 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myButton: UIButton!
+    @IBOutlet weak var myLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .green
 //        view.backgroundColor = .blue
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func didMyButtonTapped(_ sender: Any) {
+//        print("Hello jiyoung!!!")
+        myLabel.text = "Hello UIKit"
+    }
 }
 
