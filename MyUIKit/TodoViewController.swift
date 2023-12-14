b2571a400f3caa73f9ca0da008f6e9ab78552e9d
Bullets-jyr <jyr@bullets.co.kr>
2023-12-14 12:25:34 +0900

    01. NavigationController 알아보기

--- MyUIKit/TodoViewController.swift
+++ MyUIKit/TodoViewController.swift
@@ -7,23 +7,56 @@
 
 import UIKit
 
-class TodoViewController: UIViewController {
+struct TodoItem {
+    let title: String
+    var isCompleted: Bool
+}
 
+class TodoViewController: UIViewController {
+    @IBOutlet weak var todoTableView: UITableView!
+    var data: [TodoItem] = [
+        TodoItem(title: "커밋하기", isCompleted: false),
+        TodoItem(title: "운동하기", isCompleted: true)
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .gray
+        todoTableView.backgroundColor = .brown
+        todoTableView.dataSource = self
+        todoTableView.delegate = self
+    }
+}
 
-        // Do any additional setup after loading the view.
+extension TodoViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, leadingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        let myAction = UIContextualAction(style: .normal, title: "완료") { action, view, completionHandler in
+            self.data[indexPath.row].isCompleted.toggle()
+            self.todoTableView.reloadData()
+            completionHandler(true)
+        }
+        return UISwipeActionsConfiguration(actions: [myAction])
     }
-    */
-
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = todoTableView.dequeueReusableCell(withIdentifier: "todoCell", for: indexPath)
+        
+        cell.textLabel?.text = data[indexPath.row].title
+        
+        if data[indexPath.row].isCompleted {
+            cell.textLabel?.textColor = .green
+        } else {
+            cell.textLabel?.textColor = .red
+        }
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        todoTableView.deselectRow(at: indexPath, animated: true)
+    }
 }
